04e32e70fb9928da5cf16cb79d1b3250910288a8
Mishal Shah <shahmishal1998@gmail.com>
2019-08-17 10:13:09 +0530

    tests from GCI integrated

--- src/backend/tests.py
+++ src/backend/tests.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
 
 # Create your tests here.
+
+class InstallTestCase(APITestCase):
+	def install_fetch(self):
+		self.assertEqual(1, 1)
